6732dfc3f8058fa65cebc734afcc6ff7a311bad8
hemantagarwal707 <hemantagarwal707@gmail.com>
2023-04-16 23:21:35 +0530

    html page of details

--- src/Container/Details/index.js
+++ src/Container/Details/index.js
@@ -12,7 +12,7 @@ const DetailsContainer = ()=>{
     const [content, setContent] = useState();
     const [video, setVideo] = useState();
     const [credits, setCredits] = useState();
-
+    const titleName =  content && content.name && content.name !== '' ? content.name : content && content.title && content.title !== '' ?  content.title : '';
 
     const id = params.movieid || '';
     const _media_type = params && params.mediatype &&  params.mediatype !== '' ? params.mediatype.toLowerCase() : '';
@@ -53,6 +53,76 @@ const DetailsContainer = ()=>{
         creditsFetch();
         
     }, [])
+
+    
+    const renderDataHtml = ()=>{
+        const ImageURL = content.poster_path ? img_300 + content.poster_path : img_not_available;
+        const tagline = content.tagline || '';
+        const vote_average = parseInt(content.vote_average);
+        const original_language = content.original_language || '';
+        const adult = !content.adult ? '10+' : '18+';
+        const origin_country = content.origin_country && content.origin_country[0] ? content.origin_country[0] : content.production_countries && content.production_countries[0] && content.production_countries[0].name ? content.production_countries[0].name : '';
+        const overview = content.overview;
+        const first_air_date = content.first_air_date || content.release_date;
+        const  budget = content.budget || '';
+        const genres = content.genres && content.genres.length > 0 ? content.genres.map((item)=> <span  key={item.id}>{item.name}</span>) : '' ;
+        return (
+            <Row>
+                <Col className='col-12'>
+                    <h1>
+                        {titleName} 
+                        {
+                            tagline && tagline !== '' ? <small> {tagline}</small> : ''
+                        }
+                    </h1>
+                </Col>
+                <Col className='col-12 col-xl-6'>
+                    <div className='card card--details'>
+                        <div className='card__cover'>
+                            <img src={ImageURL} alt="myimage" />
+                        </div>
+                        <div className='card__content'>
+                            <div className="card__wrap">
+                                <span className="card__rate"> {vote_average}</span>
+
+                                <ul className="card__list">
+                                    <li>{original_language}</li>
+                                    <li>{adult}</li>
+                                </ul>
+                            </div>
+                            <ul className="card__meta">
+                                <li>
+                                    <span>Genre:</span> 
+                                    <span className='linkTag'>{genres}</span>
+                                </li>
+                                <li>
+                                    <span>Type:</span> 
+                                    <span className='linkTag'>{_media_type}</span>
+                                </li>
+                                
+                                <li><span>Release year:</span> <span className='linkTag'>{first_air_date}</span></li>
+                                {
+                                    budget && budget !== '' ? <li><span>Budget:- </span>
+                                    <span className='linkTag'> {budget}</span></li> : ''
+                                }
+                                
+                                <li><span>Country:</span> <span className='linkTag'>{origin_country}</span> </li>
+                            </ul>
+                            <div className="description_readmore_wrapper ">
+                                {overview}
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+                <Col className='col-12 col-xl-6'>
+                    <div className='frameSec'>
+                       
+                        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </div>
+                </Col>
+            </Row>
+        )
+    }
 
     return (
         <>
